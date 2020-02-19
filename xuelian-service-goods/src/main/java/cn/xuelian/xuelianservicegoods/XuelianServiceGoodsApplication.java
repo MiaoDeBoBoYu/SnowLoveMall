63768d3f4194f495f536a8276c4c658f20f0bf9b
boboyu <1755516725@qq.com>
2020-02-19 14:02:14 +0800

    创建好了xuelian-service-goods

---
+++ xuelian-service-goods/src/main/java/cn/xuelian/xuelianservicegoods/XuelianServiceGoodsApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian.xuelianservicegoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuelianServiceGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuelianServiceGoodsApplication.class, args);
+    }
+
+}
