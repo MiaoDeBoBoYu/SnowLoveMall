4ddca387f069f75a71bd61153a2b9cd649bf09d8
boboyu <1755516725@qq.com>
2020-02-19 13:44:20 +0800

    创建好了xuelian-bean和xuelian-interface

---
+++ xuelian-bean/src/main/java/cn/xuelian/XuelianBeanApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuelianBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuelianBeanApplication.class, args);
+    }
+
+}
---
+++ xuelian-interface/src/main/java/cn/xuelian/XueLianInterfaceApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueLianInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueLianInterfaceApplication.class, args);
+    }
+
+}
