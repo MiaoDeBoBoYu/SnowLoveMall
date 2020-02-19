3210cee8f2be41cd230a4b3144fd8ec789cffb81
boboyu <1755516725@qq.com>
2020-02-19 13:13:56 +0800

    创建好了xuelian-common-service

---
+++ xuelian-common-service/src/main/java/cn/xuelian/XuelianCommonServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuelianCommonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuelianCommonServiceApplication.class, args);
+    }
+
+}
