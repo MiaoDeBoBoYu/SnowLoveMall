bd065517a46bd99a440d2a805934e18e252fe969
boboyu <1755516725@qq.com>
2020-02-19 14:08:14 +0800

    创建好了xuelian-common-web

---
+++ xuelian-web-manager/src/main/java/cn/xuelian/XuelianWebManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuelianWebManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuelianWebManagerApplication.class, args);
+    }
+
+}
