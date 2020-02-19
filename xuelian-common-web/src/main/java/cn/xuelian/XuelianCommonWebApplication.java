8a34bcc5a737767adae7bf1e4624fd17cd5e99b2
boboyu <1755516725@qq.com>
2020-02-19 13:33:02 +0800

    创建好了xuelian-common-web

---
+++ xuelian-common-web/src/main/java/cn/xuelian/XuelianCommonWebApplication.java
@@ -0,0 +1,13 @@
+package cn.xuelian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuelianCommonWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuelianCommonWebApplication.class, args);
+    }
+
+}
